9fc029f7799248bde57de1946d60d6d3def52b81
Rab-son <rabsonsayendajnr@gmail.com>
2022-10-04 12:33:05 -0700

    template inheriting

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.base')
+
+
+@section('content')
+
+<h1 class="text-4xl font-bold"> Home </h1>
+<p class="mt-4 text-lg">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid reiciendis esse mollitia dolores sed maxime 
+    laudantium nam voluptate nulla. Numquam eaque commodi doloribus aliquam sed molestiae! Sapiente, aliquid recusandae. 
+    Aliquid.
+</p>
+
+@endsection
